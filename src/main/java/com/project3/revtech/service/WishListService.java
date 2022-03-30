e0f2ee4b8f885c442189379d62ac3404d02b7952
smei8 <smei821@gmail.com>
2022-03-30 17:10:38 -0400

    wishlist service and serviceImpl added

--- src/main/java/com/project3/revtech/service/WishListService.java
+++ src/main/java/com/project3/revtech/service/WishListService.java
@@ -7,29 +7,6 @@ import com.project3.revtech.pojo.ProductPojo;
 import com.project3.revtech.pojo.WishlistPojo;
 
 public interface WishListService {
-	
-	   WishlistPojo addWishList(WishlistPojo wishlistPojo) throws ApplicationException;
-	   
-	   WishlistPojo updateWishList(WishlistPojo wishlistPojo) throws ApplicationException;
-	   
-	   WishlistPojo getWishList(int wishlistId) throws ApplicationException;
-	   
-	   WishlistPojo getWishListByUserId(int userId) throws ApplicationException;
-	   
-	   boolean removeWishList(WishlistPojo wishListPojo) throws ApplicationException;
-	    
-	   ProductPojo addProduct(ProductPojo productPojo) throws ApplicationException;
-	    
-	   ProductPojo updateProduct(ProductPojo productPojo) throws ApplicationException;
-	    
-	   ProductPojo getWishListItem(int productId) throws ApplicationException;
-	    
-	   List<ProductPojo> getAllItemsOfWishlist(int wishlistId) throws ApplicationException;
-	    
-	   boolean removeProduct(int productId) throws ApplicationException;
-	    
-	   boolean checkIfExistsInCart(int wishlistId, int productId) throws ApplicationException;
-	    
-	   boolean checkIfNoQty(int wishlistId, int productId) throws ApplicationException;
+
 
 }
