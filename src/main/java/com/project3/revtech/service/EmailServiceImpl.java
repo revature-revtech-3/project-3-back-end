9c7c35f0cd56fc792c4c10b46343e1333e771314
TylerSBoston <98355458+TylerSBoston@users.noreply.github.com>
2022-03-30 17:20:53 -0400

    Skeletons
    Email service skelaton,
    UserSettings and UserUserSettings Pojo's skeletons added

---
+++ src/main/java/com/project3/revtech/service/EmailServiceImpl.java
@@ -0,0 +1,22 @@
+package com.project3.revtech.service;
+
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Service;
+
+
+// no interface to implement atm.
+@Service
+@Transactional
+public class EmailServiceImpl {
+	
+	
+	public void sendByDiscount(int discountID) {
+		
+	}
+	
+	public void sendByBundle(int bundleID) {
+		
+	}
+
+}
