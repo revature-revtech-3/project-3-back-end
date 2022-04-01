2f02a0959eef8fbc0e929ce823f850eadf4b930e
BenBarnhill <bconnorbarnhill@gmail.com>
2022-04-01 10:41:09 -0500

    disabled csrf

--- src/main/java/com/project3/revtech/security/WebSecurityConfig.java
+++ src/main/java/com/project3/revtech/security/WebSecurityConfig.java
@@ -61,7 +61,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
   @Override
   protected void configure(HttpSecurity http) throws Exception {
 
-	   http.cors().and()
+	   http.cors().and().csrf().disable()
 	  .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
 	  .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).
 	   and().authorizeRequests().antMatchers("/api/auth/**").permitAll().and()
