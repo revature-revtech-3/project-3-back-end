fc28ed508f7fa2dde5919b07f3eed06bdd5e0e5b
smei8 <smei821@gmail.com>
2022-04-01 12:16:10 -0400

    added wishlist qty

--- src/main/java/com/project3/revtech/entity/WishListItemEntity.java
+++ src/main/java/com/project3/revtech/entity/WishListItemEntity.java
@@ -29,23 +29,33 @@ public class WishListItemEntity {
 	@Column(name = "wish_list_id")
 	private int wishListId;
 	
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "wish_list_id", nullable = false, insertable = false, updatable = false)
-	private WishListEntity wishListEntity;
-	
 	@Column(name = "product_id")
 	private int productId;
 	
-	@Column(name = "wish_item_qty")
-	private int wishListQty;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "wish_list_id", nullable = false, insertable = false, updatable = false)
+	private WishListEntity wishListEntity;
 	
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "product_id", nullable = false, insertable = false, updatable = false)
 	private ProductEntity productEntity;
 
-	public int getUserId() {
-		// TODO Auto-generated method stub
-		return 0;
+	@Column(name = "wish_item_qty")
+	private int wishListQty;
+
+	public WishListItemEntity(int wishItemId, int wishListId, int productId, int wishListQty) {
+		super();
+		this.wishItemId = wishItemId;
+		this.wishListId = wishListId;
+		this.productId = productId;
+		this.wishListQty = wishListQty;
+	}
+
+	public WishListItemEntity(int wishListId, int productId, int wishListQty) {
+		super();
+		this.wishListId = wishListId;
+		this.productId = productId;
+		this.wishListQty = wishListQty;
 	}
 	
 	
