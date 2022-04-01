e9e307e6e0834efddd5bc0678cbee6808ecab66f
BenBarnhill <bconnorbarnhill@gmail.com>
2022-04-01 09:02:05 -0500

    removed disabled command from cors chain

--- src/main/java/com/project3/revtech/security/WebSecurityConfig.java
+++ src/main/java/com/project3/revtech/security/WebSecurityConfig.java
@@ -61,7 +61,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
   @Override
   protected void configure(HttpSecurity http) throws Exception {
 
-	   http.cors().and().csrf().disable()
+	   http.cors().and()
 	  .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
 	  .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).
 	   and().authorizeRequests().antMatchers("/api/auth/**").permitAll().and()
