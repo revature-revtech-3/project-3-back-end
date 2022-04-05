404e5b2114ec78d99bb49a4766c76ba7995b5750
kleanarev <kazi.leana@revature.net>
2022-04-05 16:45:04 -0400

    discard changes

--- src/main/java/com/project3/revtech/service/WishListServiceImpl.java
+++ src/main/java/com/project3/revtech/service/WishListServiceImpl.java
@@ -8,35 +8,36 @@ import com.project3.revtech.pojo.WishlistPojo;
 
 public class WishListServiceImpl implements WishListService {
 
-//	@Override
-//	public WishlistPojo updateWishList(WishlistPojo wishlistPojo) throws ApplicationException {
-//		// TODO Auto-generated method stub
-//		return null;
-//	}
-//
-//	@Override
-//	public ProductPojo getWishListItem(int productId) throws ApplicationException {
-//		// TODO Auto-generated method stub
-//		return null;
-//	}
-//
-//	@Override
-//	public boolean removeProduct(int productId) throws ApplicationException {
-//		// TODO Auto-generated method stub
-//		return false;
-//	}
-//
-//	@Override
-//	public void createWishList(WishlistPojo wishlistPojo) {
-//		// TODO Auto-generated method stub
-//		
-//	}
-//
-//	@Override
-//	public List<WishlistPojo> readWishList(int userId) {
-//		// TODO Auto-generated method stub
-//		return null;
-//	}
+
+	@Override
+	public WishlistPojo updateWishList(WishlistPojo wishlistPojo) throws ApplicationException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ProductPojo getWishListItem(int productId) throws ApplicationException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean removeProduct(int productId) throws ApplicationException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void createWishList(WishlistPojo wishlistPojo) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<WishlistPojo> readWishList(int userId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 	
 
