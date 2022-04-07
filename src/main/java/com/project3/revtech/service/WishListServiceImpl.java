6b1a03295899cfee18b3122f9bec34fa88e2aed3
rebeccaAtRevature <98355913+rebeccaAtRevature@users.noreply.github.com>
2022-04-07 17:47:59 -0400

    fixed wishlist merge errors

--- src/main/java/com/project3/revtech/service/WishListServiceImpl.java
+++ src/main/java/com/project3/revtech/service/WishListServiceImpl.java
@@ -76,11 +76,6 @@ public class WishListServiceImpl implements WishListService {
 		return true;
 	}
 
-	@Override
-	public WishListPojo addWishList(WishListPojo wishList) throws ApplicationException {
-		// TODO Auto-generated method stub
-		return null;
-	}
 
 }
 
