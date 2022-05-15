e4a4d993affb672e21ff59bdaa0bd6b66974a553
ryanlacombe <ryanalacombe@gmail.com>
2022-05-14 19:18:14 -0500

    Created the OrderDetails methods

--- src/main/java/com/project3/revtech/controller/OrderDetailsController.java
+++ src/main/java/com/project3/revtech/controller/OrderDetailsController.java
@@ -1,5 +1,53 @@
 package com.project3.revtech.controller;
 
+import com.project3.revtech.entity.CartEntity;
+import com.project3.revtech.entity.PurchasedItemEntity;
+import com.project3.revtech.joinedpojo.PurchasedItemProduct;
+import com.project3.revtech.pojo.PurchasedItemPojo;
+import com.project3.revtech.service.PurchasedItemService;
+import com.project3.revtech.dao.OrderDetailsRespository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@CrossOrigin
+@RequestMapping(path ="api/purchased-items")
 public class OrderDetailsController {
 
+	@Autowired
+    PurchasedItemService purchasedItemService;
+
+    /*@PostMapping("many/post")
+    boolean addManyItems(@RequestBody ArrayList<PurchasedItemPojo> items) {
+        return purchasedItemService.addMultipleItems(items);
+    }*/
+
+    /*@GetMapping("transaction/{bid}/get")
+    List<PurchasedItemProduct> getItemsByTransactionId(@PathVariable("bid") int transactionId) {
+        return purchasedItemService.getAllPurchasedProductsByTransactionId(transactionId);
+    }*/
+
+    /*@GetMapping("user/{bid}/get")
+    List<PurchasedItemProduct> getItemsByUserId(@PathVariable("bid") int userId) {
+        return purchasedItemService.getAllPurchasedProductsByUserId(userId);
+    }*/
+    
+    @GetMapping("user/order/{cid}")
+    public ArrayList<PurchasedItemEntity> findOrder(@PathVariable("cid") int cartId) {
+    	CartEntity order = OrderDetailsRespository.findCart(cartId);
+    	
+    	ArrayList<PurchasedItemEntity> returnOrder = new ArrayList<>();
+    	boolean orderChecker = order.isCartPaid();
+    	if (orderChecker = true)
+    	{
+    		PurchasedItemEntity grabOrder = OrderDetailsRespository.findOrder(cartId);
+    		returnOrder.add(grabOrder);
+    	}
+    	
+    	return returnOrder;
+    }
 }
--- src/main/java/com/project3/revtech/dao/OrderDetailsRespository.java
+++ src/main/java/com/project3/revtech/dao/OrderDetailsRespository.java
@@ -1,5 +1,30 @@
 package com.project3.revtech.dao;
 
-public interface OrderDetailsRespository {
+import com.project3.revtech.entity.CartEntity;
+import com.project3.revtech.entity.PurchasedItemEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
+public interface OrderDetailsRespository extends JpaRepository<PurchasedItemEntity, Integer> {
+	//List<PurchasedItemEntity> findAllByTransactionId(int transactionId);
+    //List<PurchasedItemEntity> findAllByUserId(int userId);
+    
+    @Query(value = "SELECT * FROM cart_details WHERE cart_id = cartId", nativeQuery = true)
+	static
+    CartEntity findCart(@Param("cartId") int cartId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+    
+    @Query(value = "SELECT * FROM purchased_items WHERE cart_id = grabCartId", nativeQuery = true)
+	static
+    PurchasedItemEntity findOrder(@Param("cartId") int grabCartId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
