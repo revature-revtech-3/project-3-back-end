a09a9c93e045187e9e43b59bd7d968187a90e48b
kiieer <christliebkiersten@gmail.com>
2022-05-17 16:16:07 -0400

    working backend

--- src/main/java/com/project3/revtech/controller/PurchasedItemsController.java
+++ src/main/java/com/project3/revtech/controller/PurchasedItemsController.java
@@ -1,6 +1,9 @@
 package com.project3.revtech.controller;
 
+import com.project3.revtech.dao.ProductDataRepository;
+import com.project3.revtech.entity.ProductDataEntity;
 import com.project3.revtech.joinedpojo.PurchasedItemProduct;
+import com.project3.revtech.pojo.ProductDataPojo;
 import com.project3.revtech.pojo.PurchasedItemPojo;
 import com.project3.revtech.service.PurchasedItemService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +20,9 @@ public class PurchasedItemsController {
 
     @Autowired
     PurchasedItemService purchasedItemService;
+    
+    @Autowired
+    ProductDataRepository productDataRepository;
 
     @PostMapping("many/post")
     boolean addManyItems(@RequestBody ArrayList<PurchasedItemPojo> items) {
@@ -34,6 +40,12 @@ public class PurchasedItemsController {
     }
     
     @GetMapping("admin/trackpurchase")
-    List<PurchasedItemProduct> getpurchases(){return purchasedItemService.getAllPurchases();};
+    List<ProductDataEntity> getPurchases(){return productDataRepository.findAllByPurchase();};
+    
+    @GetMapping("admin/tracksales")
+    List<ProductDataEntity> getSales(){return productDataRepository.findAllBySales();};
+
+    @GetMapping("admin/sum")
+    int getsum(){return productDataRepository.findSum();};
 
 }
